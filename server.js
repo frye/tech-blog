0a529f4fe9a7137ea59cbcd4b1fc8d508c1c4761
Samuli Jarvinen <frye@github.com>
2022-03-23 00:02:10 -0700

    Initial commit with placeholder content and some boilerplate

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const sequelize = require('./config/connection');
+
+const app = express();
+
+const PORT = process.env.PORT || 3001;
+
+app.use(express.static('public'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+sequelize.sync({ force: false })
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`App started and listening on port ${PORT}`);
+    });
+  });
