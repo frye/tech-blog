2515d7e6387c5cd1ee55656ebd2b0be185384d51
Samuli Jarvinen <frye@github.com>
2022-03-24 00:04:29 -0700

    Adding some content based on logged in status

--- controllers/home-controller.js
+++ controllers/home-controller.js
@@ -1,7 +1,21 @@
 const router = require('express').Router();
+const { User, Post } = require('../models');
 
 router.get('/', async (req, res) => {
-	res.render('homepage');
+
+	try {
+		const postData = await Post.findAll();
+		const posts = postData.map(post => post.get({ plain: true }));
+
+		res.render('homepage', { 
+			posts,
+			is_logged_in: req.session.is_logged_in 
+		});
+	} catch (err) {
+		console.log(err);
+		res.status(500).json({ message: err });
+	}
+
 });
 
 router.get('/login', async (req, res) => {
