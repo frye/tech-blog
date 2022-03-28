361983441c3aa26fff4dd5547bdf7a2d22b31ed0
Samuli Jarvinen <frye@github.com>
2022-03-28 02:48:39 -0700

    Testing stuff to create tables

--- models/index.js
+++ models/index.js
@@ -7,15 +7,15 @@ User.hasMany(Post, {
   onDelete: 'CASCADE'
 });
 
-Post.belongsTo(User, {
-  foreignKey: 'user_id',
-  onDelete: 'CASCADE'
-});
-
 User.hasMany(Comment, {
   foreignKey: 'user_id',
   onDelete: 'CASCADE'
 })
+
+Post.belongsTo(User, {
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE'
+});
 
 Post.hasMany(Comment, {
   foreignKey: 'post_id',
