5a076de049319367a46ca0ead3d4fb33121ace3b
Samuli Jarvinen <frye@github.com>
2022-03-30 01:17:23 -0700

    Updating model for TEXT on content fields. And adding preview to home page

--- utils/helpers.js
+++ utils/helpers.js
@@ -3,4 +3,8 @@ module.exports = {
 		// Formatting date to ISO standard and getting YYYY-MM-DD
 		return date.toISOString().slice(0, 10);
 	},
+  post_preview: str => {
+    const preview =  str.slice(0, 245);
+    return preview + '...';
+}
 };
