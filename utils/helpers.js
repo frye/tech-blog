31e24b30798278d97452234c3eae5aa3185c8dc5
Samuli Jarvinen <frye@github.com>
2022-03-30 01:42:20 -0700

    Small fixes. Time zone to local, comment heading style etc

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,8 +1,13 @@
 module.exports = {
 	format_date: (date) => {
 		// Formatting date to ISO standard and getting YYYY-MM-DD
-		return date.toISOString().slice(0, 10);
+    localDate = new Date(date + 'Z');
+    return localDate.toISOString().slice(0,10);
 	},
+  format_date_time: (date) => {
+    localDate = new Date(date + 'Z');
+    return localDate.toISOString().slice(11, 16);
+  },
   post_preview: str => {
     const preview =  str.slice(0, 245);
     return preview + '...';
